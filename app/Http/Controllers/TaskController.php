a99942415fe53fb81982bd66b555396106e90c68
Masroor Ahmad <masroor.ahmad@a1j.uk>
2025-01-13 12:58:29 +0000

    Create TaskController to handle logic

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    //
+}
