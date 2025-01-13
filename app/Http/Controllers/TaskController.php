283ad49bf58b3e4d91c0c4c3ea9ddfd071cac5b9
Masroor Ahmad <masroor.ahmad@a1j.uk>
2025-01-13 13:10:24 +0000

    Move logic into TaskController to simplify web.php

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,11 +2,38 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Task;
-
-use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     //
+    function displayTasks() { 
+        $tasks = Task::orderBy('created_at', 'asc')->get();
+        return view('tasks', [
+            'tasks' => $tasks
+        ]);
+    }
+
+    function addTask(Request $req) {
+        $validator = Validator::make($req->all(), [
+            'name' => 'required|max:255',
+        ]);
+    
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+        # Create and save task
+        $task = new Task;
+        $task->name = $req->name;
+        $task->save();
+        return redirect('/');
+    }
+
+    function deleteTask($id) {
+        Task::findOrFail($id)->delete();
+        return redirect('/');
+    }
 }
